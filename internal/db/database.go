03f3edce3f7388b4104b6502caa0de086452f7df
Rishab Karki <nullrish@gmail.com>
2026-03-05 09:19:22 +0545

    Added context aware database ping

--- internal/db/database.go
+++ internal/db/database.go
@@ -1,9 +1,12 @@
 package db
 
 import (
+	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 	"log"
+	"time"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
@@ -14,8 +17,13 @@ func InitializeDatabase(connString string) *sql.DB {
 	if err != nil {
 		log.Fatal("❌ Failed to initialize database:", err.Error())
 	}
-	if err := db.Ping(); err != nil {
-		log.Fatal("❌ Failed to open database:", err.Error())
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	if err := db.PingContext(ctx); err != nil {
+		if errors.Is(err, context.DeadlineExceeded) || errors.Is(err, context.Canceled) {
+			err = ctx.Err()
+		}
+		log.Fatal("❌ Failed to open database: ", err.Error())
 	}
 	log.Println("✅ Successfully opened database!")
 	fmt.Println()
